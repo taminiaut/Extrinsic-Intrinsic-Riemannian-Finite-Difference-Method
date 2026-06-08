d351efedb5ed1f599cb94144408df3fa22623d05
Timothé Taminiau <timothe.taminiau@hotmail.com>
2026-06-08 10:48:41 +0200

    Small typo

--- ambientlincomb.m
+++ ambientlincomb.m
@@ -2,7 +2,7 @@
 % two vectors lying in the ambient space, used by the Extrinsic method.
 % Note that the inputs d1 and d2 must be represented as vectors in the 
 % ambient space corresponding to a matrix space or a product of matrix spaces.
-% It is not always the case in manopt (see, e.g. fixed-rank manifold),
+% It is not always the case in manopt (see, e.g., the fixed-rank manifold),
 % hence few transformations of the input may be needed in the function below.
 
 function res = ambientlincomb(x, a1, d1, a2, d2, M)
@@ -20,11 +20,11 @@ end
 
 
 % If you are working with the fixed-rank manifold R^{m x n}_k,
-% you should add the following:
+% then you should add the following:
 
-% fullMat1 = problem.M.triplet2matrix(d1); % From SVD to full matrix
-% fullMat2 = problem.M.triplet2matrix(d2); % From SVD to full matrix
+% fullMat1 = M.triplet2matrix(d1); % From SVD to full matrix
+% fullMat2 = M.triplet2matrix(d2); % From SVD to full matrix
 % matRes = matrixlincomb(x,a1,fullMat1,a2,fullMat2);
     
-% res = problem.M.matrix2triplet(matRes); % From full matrix to SVD
-% The last line may not be needed if f can take as input matrices
+% res = M.matrix2triplet(matRes); % From full matrix to SVD
+% The last line is not needed if f(.) can take as input the full matrix.
